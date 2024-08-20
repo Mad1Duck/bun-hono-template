2d1d65bdcf7f1eba642f738148fae91c6c757237
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-20 12:16:14 +0700

    perbaikan

--- src/bin/database.ts
+++ src/bin/database.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
